80af5cb45d1f53bcc389daa3b7b671a9dbc1175b
VlaDSerga <65920800+VlaDSerga@users.noreply.github.com>
2026-03-16 08:58:15 +0300

    commit header mnu

--- js/functions.js
+++ js/functions.js
@@ -36,6 +36,90 @@ $(() => {
 	$(':root').css('--scroll_width', widthScroll() + 'px')
 
 
+	$('body').on('click', '.header-catalog__open', function (e) {
+		e.preventDefault()
+
+		if ($(this).hasClass('_active')) {
+			$(this).removeClass('_active')
+			$('.header-catalog__block').removeClass('_show')
+			$('.overlay-catalog').removeClass('_show')
+		} else {
+			$(this).addClass('_active')
+			$('.header-catalog__block').addClass('_show')
+			$('.overlay-catalog').addClass('_show')
+		}
+	})
+
+	// Закрываем всплывашку при клике за её пределами
+	$(document).click((e) => {
+		if ( !e.target.closest('.header-catalog') && !e.target.closest('.header-catalog__open') ) {
+			$('.header-catalog__open').removeClass('_active')
+			$('.header-catalog__block').removeClass('_show')
+			$('.overlay-catalog').removeClass('_show')
+		}
+	})
+
+	// $('.header-menu__item').hover(function() {
+	// 	// $(this).addClass('_active');
+
+
+	// 	if( !$(this).hasClass('_active') ) {
+	// 		let parent = $(this).closest('.header-catalog__block')
+	// 		let activeTab = $(this).data('content')
+
+	// 		parent.find('.header-menu:first').find('.menu_js').removeClass('_active')
+
+	// 		$(this).addClass('_active')
+	// 		$(activeTab).addClass('_active')
+	// 	}
+	// }, function() {
+	// 	$(this).removeClass('_active');
+	// });
+
+
+	// Табы
+	// var locationHash = window.location.hash
+
+	// $('body').on('click', '.tabs__button_js', function(e) {
+	// 	e.preventDefault()
+
+	// 	if( !$(this).hasClass('_active') ) {
+	// 		let parent = $(this).closest('.tabs-container')
+	// 		let activeTab = $(this).data('content')
+	// 		let level = $(this).data('level')
+
+	// 		parent.find('.tabs:first').find('.tabs__button_js').removeClass('_active')
+	// 		parent.find('.tab-content.' + level).removeClass('_active')
+
+	// 		$(this).addClass('_active')
+	// 		$(activeTab).addClass('_active')
+
+	// 		if ( $(this).closest('.production-process').length ) {
+	// 			clearTimeout($stopTimer);
+	// 			$index = $(this).index();
+	// 			$('.production-process .tabs').attr('class', '').addClass('tabs act' + $index);
+	// 			tabsTimer();
+
+	// 			return false
+	// 		}
+	// 	}
+	// })
+
+	// if( locationHash && $('.tabs-container').length ) {
+	// 	let activeTab = $('.tabs__button_js[data-content="'+ locationHash +'"]')
+	// 	let parent = activeTab.closest('.tabs-container')
+	// 	let level = activeTab.data('level')
+
+	// 	parent.find('.tabs:first').find('.tabs__button_js').removeClass('_active')
+	// 	parent.find('.tab-content.' + level).removeClass('_active')
+
+	// 	activeTab.addClass('_active')
+	// 	$(locationHash).addClass('_active')
+
+	// 	$('html, body').stop().animate({
+	// 		scrollTop: $(locationHash).offset().top - 120
+	// 	}, 1000)
+	// }
 
 	// commit
 
